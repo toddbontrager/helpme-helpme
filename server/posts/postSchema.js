f44281c7616b930d3d62755e8843866949f1fde8
Rocky Murdoch <rockysta009@gmail.com>
2016-04-08 16:32:21 -0700

    (fix) rename update to post and fix styling

--- server/posts/postSchema.js
+++ server/posts/postSchema.js
@@ -3,12 +3,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var PostSchema = new Schema({
-    update: {
-        type: String,
-        required: true
-    }
-}, {
-    timestamps: true
+  post: { type: String, required: true }
+},
+{
+  timestamps: true
 });
 
 module.exports = PostSchema;
