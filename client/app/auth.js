ae25430e1462114474f507da8e84e5c4e604d7fe
Kevin Chen <kevnchn@gmail.com>
2016-04-08 18:34:59 -0400

    (feat) Fill out controller and factory for signin/signup

--- client/app/auth.js
+++ client/app/auth.js
@@ -1,5 +1,28 @@
 var Auth = angular.module('app.auth', []);
 
-Auth.controller('AuthController', function($scope, $location) {
+Auth.controller('AuthController', function($scope, $state, $window, Auth) {
+  $scope.user = {};
+  $scope.signin = function() {
+    Auth.signin($scope.user)
+      .then(function(token) {
+        // authentication here:
+        // $window.localStorage.setItem('com.helpme', token);
+        $state.go('/app.main');
+      })
+      .catch(function(error) {
+        console.error(error);
+      });
+  };
 
+  $scope.signup = function() {
+    Auth.signup($scope.user)
+      .then(function(token) {
+        // authentication here:
+        // $window.localStorage.setItem('com.helpme', token);
+        $state.go('/app.main');
+      })
+      .catch(function(error) {
+        console.error(error);
+      });
+  };
 });
--- client/app/services.js
+++ client/app/services.js
@@ -1,5 +1,36 @@
 var Service = angular.module('app.services', []);
 
-Service.controller('AppController', function($scope, $location) {
+Service.factory('Auth', function($http, $state, $window) {
+  return {
+    signin: function(user) {
+      return $http({
+          method: 'POST',
+          url: '/api/signin',
+          data: user
+        })
+        .then(function(res) {
+          // return res.data.token;
+        });
+    },
 
+    signup: function(user) {
+      return $http({
+          method: 'POST',
+          url: '/api/signup',
+          data: user
+        })
+        .then(function(res) {
+          // return res.data.token;
+        });
+    },
+
+    isAuth: function() {
+      // return !!$window.localStorage.getItem('com.helpme');
+    },
+
+    signout: function() {
+      // $window.localStorage.removeItem('com.helpme');
+      $state.go('signin');
+    }
+  };
 });
