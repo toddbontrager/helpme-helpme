d2d7b231a0cb9531349722a7dfb4648e8f87eaaa
Aaron Hildebrand <aaron.hildebrand@gmail.com>
2016-04-08 12:15:59 -0700

    (feat) Update app.js with default routing and states.

--- client/app/app.js
+++ client/app/app.js
@@ -1,1 +1,47 @@
-angular.module('app', []);
+var app = angular.module('app', ['app.auth', 'app.services', 'ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/signin');
+
+  $stateProvider
+    .state('signin', {
+      url: '/signin',
+      templateUrl: 'app/partials/partial-signin.html',
+      controller: 'AuthController'
+    })
+    .state('signup', {
+      url: '/signup',
+      templateUrl: 'app/partials/partial-signup.html',
+      controller: 'AuthController'
+    })
+    .state('app', {
+      url: '/app',
+      templateUrl: 'app/partials/partial-app.html',
+      controller: 'AppController',
+      authenticate: true
+    })
+      .state('app.main', {
+        url: '/main',
+        templateUrl: 'app/partials/partial-app-main.html',
+        controller: 'AppController',
+        authenticate: true
+      })
+      .state('app.goal', {
+        url: '/goal',
+        templateUrl: 'app/partials/partial-app-goal.html',
+        controller: 'AppController',
+        authenticate: true
+      })
+      .state('app.friends', {
+        url: '/friends',
+        templateUrl: 'app/partials/partial-app-friends.html',
+        controller: 'AppController',
+        authenticate: true
+      })
+      .state('app.profile', {
+        url: '/profile',
+        templateUrl: 'app/partials/partial-app-profile.html',
+        controller: 'AppController',
+        authenticate: true
+      });
+});
