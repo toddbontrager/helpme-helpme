87221ab64778b6dc74112037cfe406a3868ac52f
Rocky Murdoch <rockysta009@gmail.com>
2016-04-08 17:38:58 -0700

    (refactor) separate the middleware and routing logic from server

--- server/server.js
+++ server/server.js
@@ -1,22 +1,17 @@
 var express = require('express');
 var mongoose = require('mongoose');
-var bodyParser = require("body-parser");
+
 var app = express();
 
 mongoose.connect('mongodb://localhost/helpme');
 
-
-// files to serve
-// app.use(express.static());
-
-
-// get data from a POST
-// app.use(bodyParser.  );
+// middleware and routing configuration
+require('./config/middleware.js')(app, express);
+require('./config/route.js')(app, express);
 
 var port = process.env.PORT || 8080;
 
-
 app.listen(port);
-console.log('Server is running on' + port);
+console.log('Server is running on ' + port);
 
 module.exports = app;
