b3e4e2d71958c01a4da489f2a81ff8ea72d5c4e0
Rocky Murdoch <rockysta009@gmail.com>
2016-04-08 17:59:25 -0700

    (refactor/feat) group routes, add POST goal and error handler

---
+++ server/config/route.js
@@ -0,0 +1,17 @@
+var goalController = require('../goals/goalController.js');
+var userController = require('../users/userController.js');
+var postController = require('../posts/postController.js');
+var helpers = require('./helper.js');
+
+module.exports = function(app, express) {
+
+app.route('/goals')
+  .get(goalController.allGoals)
+  .post(goalController.newGoal);
+
+  // If a request is sent somewhere other than the routes above,
+  // send it through our custom error handler
+  app.use(helpers.errorLogger);
+  app.use(helpers.errorHandler);
+
+};
