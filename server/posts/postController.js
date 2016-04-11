382e0de7ad4e8f6ce85821be7526c4d65ebc2ce5
Kevin Chen <kevnchn@gmail.com>
2016-04-11 19:07:45 -0400

    (feat) Add handler for profile GET request

--- server/posts/postController.js
+++ server/posts/postController.js
@@ -1,6 +1,16 @@
 var User = require('../users/userModel');
 
 module.exports = {
+  getProfile: function(req, res) {
+    var username = req.body.username;
+
+    User.findOne({ username: username })
+      .then(function(user) {
+        user.save();
+        res.status(200).json(user);
+      });
+  },
+
   addPost: function(req, res) {
     var post = req.body.post;
     var goalId = req.body.goalId;
@@ -17,7 +27,7 @@ module.exports = {
       })
       .then(function(user) {
         user.save();
-        res.status(200).json(user.goals.id(goalId));
+        res.status(201).json(user.goals.id(goalId));
       });
   }
 };
