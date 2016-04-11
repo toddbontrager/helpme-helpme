275e90ee747d9b1d286be63eaa2b6c1dcc849728
Rocky Murdoch <rockysta009@gmail.com>
2016-04-11 12:18:39 -0700

    (feat) add title and descripton on addGoal function

--- server/goals/goalController.js
+++ server/goals/goalController.js
@@ -2,25 +2,36 @@ var User = require('../users/userModel.js');
 
 module.exports = {
   allGoals: function(req, res) {
-    // TO DO
+    // TO DO - how to find username or id of logged in user - Auth0
+    // Coordinate with Aaron
+    // 
+    // var username = req.body.username;
+    
+    // User.findOne({ username: username })
+    //   .then(function(user) {
+    //     res.json(user.goals);
+    //   });
   },
 
-  newGoal: function(req, res) {
-    var goal = req.body.goal;
+  addGoal: function(req, res) {
+    var title = req.body.title;
+    var description = req.body.description;
     var username = req.body.username;
-    var dueDate = req.body.dueDate;
+    var dueDate = new Date(req.body.dueDate);
 
     User.findOne({ username: username })
       .then(function(user) {
-        user.goals.push({
-          goal: goal,
-          due_date: dueDate
-        });
+          user.goals.push({
+            title: title,
+            description: description,
+            due_date: dueDate
+          });
         return user;
       })
       .then(function(user) {
         user.save();
-        res.status(200).send('Goal added');
+        console.log(user);
+        res.status(200).json(user.goals);
       });
   }
 };
