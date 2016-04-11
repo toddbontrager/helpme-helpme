217e28e5b62da27218dc20e392a87e686873d697
Rocky Murdoch <rockysta009@gmail.com>
2016-04-11 11:28:38 -0700

    (feat) replace goals with title and description - goalSchema

--- server/goals/goalSchema.js
+++ server/goals/goalSchema.js
@@ -4,10 +4,8 @@ var PostSchema = require('../posts/postSchema.js');
 var Schema = mongoose.Schema;
 
 var GoalSchema = new Schema({
-  goal: {
-    type: String,
-    required: true
-  },
+  title: { type: String, required: true },
+  description: { type: String },
   due_date: { type: Date },
   posts: [PostSchema]
 },
