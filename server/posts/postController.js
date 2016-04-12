3ac8e315f3b71146d10756044533fb20152fc24b
Kevin Chen <kevnchn@gmail.com>
2016-04-12 01:49:09 -0400

    (fix) Change profile route handlers to use user_id

--- server/posts/postController.js
+++ server/posts/postController.js
@@ -2,11 +2,10 @@ var User = require('../users/userModel');
 
 module.exports = {
   getProfile: function(req, res) {
-    var username = req.body.username;
+    var user_id = req.body.user_id;
 
-    User.findOne({ username: username })
+    User.findOne({ user_id: user_id })
       .then(function(user) {
-        user.save();
         res.status(200).json(user);
       });
   },
@@ -14,9 +13,9 @@ module.exports = {
   addPost: function(req, res) {
     var post = req.body.post;
     var goalId = req.body.goalId;
-    var username = req.body.username;
+    var user_id = req.body.user_id;
 
-    User.findOne({ username: username })
+    User.findOne({ user_id: user_id })
       // find goal by id and add new post to goal
       .then(function(user) {
         var goal = user.goals.id(goalId);
