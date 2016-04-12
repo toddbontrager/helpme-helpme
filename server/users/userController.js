26e47fc5a1a3e87c23a52734a2686131514255fa
Rocky Murdoch <rockysta009@gmail.com>
2016-04-11 21:30:09 -0700

    (refactor) impleneted friendRequest and getFriendship functions for re-use

--- server/users/userController.js
+++ server/users/userController.js
@@ -1,41 +1,51 @@
 var User = require('./userModel');
 var Status = require('mongoose-friends').Status;
 
+// moongoose-friends docs https://www.npmjs.com/package/mongoose-friends
+var friendRequest = function(userSendingRequest, friendBeingAdded, res, next) {
+  User.requestFriend(userSendingRequest, friendBeingAdded, function(err, friendship) {
+    if (err) {
+      next(new Error(err));
+    } else {
+      res.status(200).json(friendship);
+    }
+  });
+};
+
+var getFriendship = function(userId, status, res, next) {
+  User.getFriends(userId, status, function(err, friendship) {
+    if (err) {
+      next(new Error(err));
+    } else {
+      res.status(200).json(friendship);
+    }
+  });
+};
+
 module.exports = {
-  sendFriendRequest: function(req, res) {
-    var friendId = req.body.friendId;
-    var userId = req.body.userId;
-    var requested = { 'friends.status': Status.Requested };
-
-    User.requestFriend(userId, friendId);
-
-    // TO DO - json not sending but its adding
-    User.getFriends(userId, requested, function(err, requestedFriends) {
-      res.status(200).json(requestedFriends);
-    });
-  },
-
-  acceptFriendRequest: function(req, res) {
+  sendFriendRequest: function(req, res, next) {
     var userId = req.body.userId;
     var friendId = req.body.friendId;
+    friendRequest(userId, friendId, res, next);
+  },
+
+  acceptFriendRequest: function(req, res, next) {
 
   },
 
-  allFriends: function(req, res) {
+  allFriends: function(req, res, next) {
 
   },
 
-  getFriendRequests: function(req, res) {
+  getFriendRequests: function(req, res, next) {
+    // TO DO - how to get Id of user logged in
+    var userId = req.body.userId;
     var pending = { 'friends.status': Status.Pending };
-      // TO DO - how to get Id of user logged in
-    var userId = req.body.userId;
 
-    User.getFriends(userId, pending, function(err, friendRequests) {
-      res.status(200).json(friendRequests);
-    });
+    getFriendship(userId, pending, res, next);
   },
 
-  getRequestedFriends: function(req, res) {
+  getRequestedFriends: function(req, res, next) {
 
   }
 };
