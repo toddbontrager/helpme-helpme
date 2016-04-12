8e724ac68be4f7cd7d7aa9bd27b4546254fcb5bb
Kevin Chen <kevnchn@gmail.com>
2016-04-12 15:19:31 -0400

    (feat) Update request handling for goals api

--- server/goals/goalController.js
+++ server/goals/goalController.js
@@ -1,36 +1,25 @@
 var User = require('../users/userModel.js');
 
 module.exports = {
-  allGoals: function(req, res) {
-    // TO DO - how to find username or id of logged in user - Auth0
-    // Coordinate with Aaron
-    // 
-    // var username = req.body.username;
-    
-    // User.findOne({ username: username })
-    //   .then(function(user) {
-    //     res.json(user.goals);
-    //   });
+  getGoals: function(req, res) {
+    var user_id = req.params.user_id;
+
+    User.findOne({ auth_id: user_id })
+      .then(function(user) {
+        var goals = user.goals;
+        res.status(200).json(goals);
+      });
   },
 
   addGoal: function(req, res) {
-    var title = req.body.title;
-    var description = req.body.description;
-    var username = req.body.username;
-    var dueDate = new Date(req.body.dueDate);
+    var user_id = req.params.user_id;
+    var goal = req.body;
 
-    User.findOne({ username: username })
+    User.findOne({ auth_id: user_id })
       .then(function(user) {
-        user.goals.push({
-          title: title,
-          description: description,
-          due_date: dueDate
-        });
-        return user;
-      })
-      .then(function(user) {
+        user.goals.push(goal);
         user.save();
-        res.status(200).json(user.goals);
+        res.status(201).json(user.goals);
       });
   }
 };
