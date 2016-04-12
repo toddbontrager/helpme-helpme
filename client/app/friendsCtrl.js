0cd49e0f2af0536fedc6b0ceee2d8f71b0700be9
Aaron Hildebrand <aaron.hildebrand@gmail.com>
2016-04-12 15:10:33 -0700

    (feat) Add factory to friendsCtrl.js.

--- client/app/friendsCtrl.js
+++ client/app/friendsCtrl.js
@@ -1,6 +1,49 @@
 var Friends = angular.module('app.friends', []);
 
-Friends.factory()
+Friends.factory('Friend', function($http) {
+  var getFriends = function(user_id) {
+    return $http({
+      method: 'GET',
+      url: '/api/friends'
+    })
+    .then(function(res) {
+      return res.data;
+    })
+  };
+
+  var addFriend = function(friendship) {
+    return $http({
+      method: 'POST',
+      url: '/api/friends/add',
+      data: friendship
+    })
+    .then(function(res) {
+      return res.data;
+    })
+  };
+
+  var removeFriend = function(friendship) {
+    return $http({
+      method: 'POST',
+      url: '/api/friends/remove',
+      data: friendship
+    })
+    .then(function(res) {
+      return res.data;
+    })
+  };
+
+  var answerFriendReq = function(answer) {
+    return $http({
+      method: 'POST',
+      url: '/api/friends/requests',
+      data: answer
+    })
+    .then(function(res) {
+      return res.data;
+    })
+  };
+})
 
 Friends.controller('FriendsController', function($scope, auth) {
   // User profile information from Auth0 db
