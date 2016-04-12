4f91b5308f33f9ebdf2c6f8449d3ad89e5af2785
Rocky Murdoch <rockysta009@gmail.com>
2016-04-12 12:24:55 -0700

    (feat) add sendJSON helper function

--- server/config/helper.js
+++ server/config/helper.js
@@ -1,4 +1,12 @@
 module.exports = {
+  
+  sendJSON: function(error, data, res, next) {
+    if (error) {
+      next(new Error(error));
+    } else {
+      res.status(200).json(data);
+    }
+  },
   errorLogger: function (error, req, res, next) {
     // log the error then send it to the next middleware in
     console.error(error.stack);
