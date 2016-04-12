526a853c0170813bf192a3da8490cc71563d85c4
Rocky Murdoch <rockysta009@gmail.com>
2016-04-11 19:46:43 -0700

    (feat) add mongoose-friends to UserSchema definition

--- server/users/userModel.js
+++ server/users/userModel.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var friends = require('mongoose-friends');
 var GoalSchema = require('../goals/goalSchema.js');
 
 var Schema = mongoose.Schema;
@@ -9,10 +10,19 @@ var UserSchema = new Schema({
   firstname: { type: String, required: true },
   lastname: { type: String, required: true },
   goals: [GoalSchema],
-  friends: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+  /*
+  friends: [{
+    added: The date the friendship request was first *created* (NOT accepted
+    status: pending/accepted/requested
+    _id: ID of friend
+  }]
+  */
 },
 {
   timestamps: true
 });
+
+UserSchema.plugin(friends({pathName: 'friends'}));
+// mongoose-friends automatically adds a friends array defined above
 
 module.exports = mongoose.model('User', UserSchema);
