3ac37ca238fbf44d96a1b17e579a5353d63ed20a
Aaron Hildebrand <aaron.hildebrand@gmail.com>
2016-04-12 17:09:49 -0700

    (feat) Add remaining controller functions. Update partial-app-friends to conditionally display pending friend requests and searched friends.

--- client/app/friendsCtrl.js
+++ client/app/friendsCtrl.js
@@ -44,6 +44,16 @@ Friends.factory('Friend', function($http) {
     });
   };
 
+  var getPendingReqs = function(user_id) {
+      return $http({
+        method: 'GET',
+        url: '/api/friends/pending/' + user_id
+      })
+      .then(function(res) {
+        return res.data;
+      });
+    };
+
   var answerFriendReq = function(user_id, answer) {
     return $http({
       method: 'POST',
@@ -60,6 +70,7 @@ Friends.factory('Friend', function($http) {
     getFriends: getFriends,
     addFriend: addFriend,
     removeFriend: removeFriend,
+    getPendingReqs: getPendingReqs,
     answerFriendReq: answerFriendReq
   };
 });
@@ -73,14 +84,15 @@ Friends.controller('FriendsController', function($scope, auth, Friend) {
   $scope.input = {};
 
   $scope.searchData = {};
+  $scope.showSubmitted = false;
+  $scope.isPending = false;
 
   $scope.searchFriend = function() {
     var input = $scope.input;
-    var user_id = $scope.profile.user_id;
-    Friend.searchfriend(user_id, input)
+    Friend.searchFriend($scope.profile.user_id, input)
       .then(function(data) {
         console.log(data);
-        $scope.searchData = data;
+        $scope.searchData.users = data;
         for(var key in $scope.input) {
           $scope.input[key] = '';
         }
@@ -89,5 +101,63 @@ Friends.controller('FriendsController', function($scope, auth, Friend) {
         console.error(error);
       });
   };
+
+  $scope.getFriends = function() {
+    Friend.getFriends($scope.profile.user_id)
+      .then(function(data) {
+        $scope.user.friends = data;
+      })
+      .catch(function(error) {
+        console.error(error);
+      });
+  };
+
+  $scope.addFriend = function(friend_id) {
+    Friend.addFriend($scope.profile.user_id, friend_id)
+      .then(function(data) {
+        $scope.showSubmitted=true;
+        setTimeout(function(){$scope.showSubmitted=false;},5000);
+        $scope.isAddFriendOpen=false;
+      })
+      .catch(function(error) {
+        console.error(error);
+      });
+  };
+
+  $scope.removeFriend = function(friend_id) {
+    Friend.removeFriend($scope.profile.user_id, friend_id)
+      .then(function(data) {
+        $scope.getFriends();
+      })
+      .catch(function(error) {
+        console.error(error);
+      });
+  };
+
+  $scope.getPendingReqs = function() {
+    Friend.getPendingReqs($scope.profile.user_id)
+      .then(function(data) {
+        $scope.user.pending = data;
+        if($scope.user.pending.length > 0) {
+          $scope.isPending = true;
+        }
+      })
+      .catch(function(error) {
+        console.error(error);
+      });
+  };
+
+  $scope.answerFriendReq = function(answer) {
+    Friend.answerFriendReq($scope.profile.user_id, answer)
+      .then(function(data) {
+        $scope.getPendingReqs();
+      })
+      .catch(function(error) {
+        console.error(error);
+      });
+  };
+
+  $scope.getPendingReqs();
+  $scope.getFriends();
 
 });
