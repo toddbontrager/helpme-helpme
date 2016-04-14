9f4630a87b27c8eb5f1f3878705715e2dd38e382
Aaron Hildebrand <aaron.hildebrand@gmail.com>
2016-04-14 14:53:11 -0700

    (feat) Add support for angular-moment. Update modules and partials to utilize angular-moment.

--- client/app/goals/goalsCtrl.js
+++ client/app/goals/goalsCtrl.js
@@ -1,5 +1,5 @@
 angular
-  .module('app.goals', [])
+  .module('app.goals', ['angularMoment'])
   .controller('GoalsController', GoalsController);
 
 GoalsController.$inject = ['$scope', 'auth', 'Goals'];
