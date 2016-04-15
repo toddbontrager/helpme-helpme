26cae95b4bade546071b500304d3d9b094d709cd
Rocky Murdoch <rockysta009@gmail.com>
2016-04-14 22:28:20 -0700

    (feat) implemented addComment method on mainCtrl

--- client/app/main/mainCtrl.js
+++ client/app/main/mainCtrl.js
@@ -2,13 +2,12 @@ angular
   .module('app.main', [])
   .controller('MainController', MainController);
 
-MainController.$inject = ['$scope', 'auth', 'Goals', 'Friend'];
+MainController.$inject = ['$scope', 'auth', 'Goals', 'Friend', 'Profile'];
 
-function MainController($scope, auth, Goals, Friend) {
+function MainController($scope, auth, Goals, Friend, Profile) {
   $scope.profile = auth.profile;
   // User information from our MongoDB
   $scope.user = {};
-  // Form input fields
 
   $scope.getGoals = function() {
     Goals.getGoals($scope.profile.user_id)
@@ -39,11 +38,30 @@ function MainController($scope, auth, Goals, Friend) {
           friend.firstname = obj[0].firstname || '';
           friend.lastname = obj[0].lastname || '';
           friend.username = obj[0].username || '';
+          friend.auth_id = obj[0].auth_id;
           obj[1].forEach(function(post) {
             post.friend = friend;
             $scope.posts.push(post);
           });
         });
+        console.log($scope.posts);
+      })
+      .catch(function(error) {
+        console.error(error);
+      });
+  };
+
+  $scope.addComment = function(post_id, goal_id, input, friend_id) {
+    var user_id = $scope.profile.user_id;
+    Profile.addComment(user_id, goal_id, post_id, input, friend_id)
+      .then(function(data) {
+        for(var i = 0; i < $scope.posts.length; i++) {
+          var post = $scope.posts[i];
+          if (post.goal_id === data.goal_id) {
+            post.comments = data.comments;
+            return;
+          }
+        }
       })
       .catch(function(error) {
         console.error(error);
