3815f51210d51f9f19f262ef643d71692a50cd9f
Rob Bruhn <rdbruhn@gmail.com>
2016-04-22 21:16:34 -0500

    (fix) Add and change properties on guidSchema to match front-end

--- server/guides/guideSchema.js
+++ server/guides/guideSchema.js
@@ -2,9 +2,11 @@ var mongoose = require('mongoose');
 
 var guideSchema = new mongoose.Schema({
   name: String,
+  author: String,
   category: [String],
   file: String,
-  icon: String
+  icon: String,
+  description: String,
 });
 
 var Guide = mongoose.model('guide', guideSchema);
